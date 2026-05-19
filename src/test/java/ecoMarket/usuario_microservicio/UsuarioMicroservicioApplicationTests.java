f9b801d689a3ce35ff941823937f14b73e0b4dab
PedroSuarez91 <pe.suarez@duocuc.cl>
2026-05-18 21:20:41 -0400

    creacionde servicio usuario

---
+++ src/test/java/ecoMarket/usuario_microservicio/UsuarioMicroservicioApplicationTests.java
@@ -0,0 +1,13 @@
+package ecoMarket.usuario_microservicio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioMicroservicioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
