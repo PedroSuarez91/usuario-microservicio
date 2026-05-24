e4707a38896d9b9f1a04c7cb8f43ecac904fcf6b
PedroSuarez91 <pe.suarez@duocuc.cl>
2026-05-24 11:08:18 -0400

    cambios

--- src/main/java/ecoMarket/usuario_microservicio/Repository/UsuarioRepository.java
+++
@@ -1,9 +0,0 @@
-package ecoMarket.usuario_microservicio.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import ecoMarket.usuario_microservicio.model.Usuario;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
-    
-}
